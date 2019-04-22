92c8dd61bb2ce9a7faa26bd8be2e5616a624d4eb
guan <hwzhxf_kf@qq.com>
2019-04-22 11:02:30 +0800

    zuul config

---
+++ eureka-zuul-client/src/main/java/demo/springcloud/eurekazuulclient/MyFallBackProvider.java
@@ -0,0 +1,57 @@
+package demo.springcloud.eurekazuulclient;
+
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+@Component
+public class MyFallBackProvider implements ZuulFallbackProvider {
+    @Override
+    public String getRoute() {
+        //所有路由的返回方法 可用*
+        return "eureka-client";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("ooops,error! this is fallback".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+                return httpHeaders;
+            }
+        };
+    }
+}
